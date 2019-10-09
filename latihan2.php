ba56c4f9722797438c1c515405b95294e667d553
Nivia Nur Kharisma <53246336+NiviaNurKharisma@users.noreply.github.com>
2019-10-09 08:52:22 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,27 @@
+<?php
+//Looping / Pengulangan
+
+//Type 1 = FOR
+//for($i=1; $i <= 10; $i++){
+    //echo $i . "Nivia Nur Kharisma <br>";
+//}
+
+//Type 2 = WHILE
+//$i = 1;
+//while ($i <= 10){
+    //echo $i . "Nivia Nur Kharisma <br>";
+   // $i++;
+//}
+
+//Type 3 = FOREACH
+// Conditional / Pengkodisian
+//$angka = 10;
+//if ($angka > 10){
+    //echo "Lebih besar dari 10";
+//}else if ($angka == 10){
+    //echo " Sama dengan 10";
+//}
+//else{
+    //echo "Kurang dari 10";
+//}
+?>
